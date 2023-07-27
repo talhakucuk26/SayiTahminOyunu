53322298db5380973a80d545ba262838f7bfd51f
Talha <talhakucuk26@gmail.comgit config --global user.name Talhagit commitgit config --global user.email talhakucuk26@gmail.com>
2023-07-27 19:55:43 +0300

    Sayı Tahmin Oyunu.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int right = 0, selected;
+        boolean isWin = false, isWrong = false;
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        Scanner input = new Scanner(System.in);
+
+        int[] wrong = new int[5];
+
+        while (right < 5) {
+            System.out.print("Tahmininiz : ");
+            selected = input.nextInt();
+
+            if (selected == number) {
+                System.out.println("Gizli sayı: " + number);
+                System.out.println("Tebrikler, doğru tahmin! Tahmin ettiğini sayı : " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Maalesef yanlış tahmin!");
+
+                wrong[right++] = selected;
+                System.out.println("HKalan Hak : " + (5 - right));
+            }
+        }
+
+        if (!isWin) {
+            System.out.println("Kaybettiniz ! ");
+            System.out.println("Gizli sayı: " + number);
+            if (!isWrong) {
+                System.out.println("Tahminler : " + Arrays.toString(wrong));
+            }
+        }
+
+    }
+}
